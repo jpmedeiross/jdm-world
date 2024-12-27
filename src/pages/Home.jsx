50ecd4122a01584bcd07139b43bfc6927aadb0ab
João Pedro Medeiros <jpmdrsdev@gmail.com>
2024-12-27 17:45:25 -0300

    changing home title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
     return (
         <div className="text-center p-4">
-            <h1>Bem-vindo ao JDM World!</h1>
+            <h1>Bem-vindo ao universo JDM: onde a paixão por carros japoneses começa!</h1>
             <p>Explore carros lendários da cultura japonesa.</p>
             <Link to={'/cars'}>
                 <button className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
